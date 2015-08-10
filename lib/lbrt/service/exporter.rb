1ea83a52eb2f3e281b34760651937d5f36e488cd
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-08-10 23:17:10 +0900

    Implement service exporter

--- lib/lbrt/service/exporter.rb
+++ lib/lbrt/service/exporter.rb
@@ -1,4 +1,8 @@
 class Lbrt::Service::Exporter
+  EXCLUDE_KEYS = %w(
+    id
+  )
+
   class << self
     def export(client, options = {})
       self.new(client, options).export
@@ -11,6 +15,27 @@ class Lbrt::Service::Exporter
   end
 
   def export
-    # XXX:
+    services = @client.services.get
+    normalize(services)
+  end
+
+  def normalize(services)
+    service_by_title = {}
+
+    services.each do |s|
+      title = s.delete('title')
+
+      if service_by_title[title]
+        raise "Duplicate service title exists: #{title}"
+      end
+
+      EXCLUDE_KEYS.each do |key|
+        s.delete(key)
+      end
+
+      service_by_title[title] = s
+    end
+
+    service_by_title
   end
 end
